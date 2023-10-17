a99d241fb03b87a6f0af7e7d758dfaa26f94de96
toom1996 <1023150697@qq.com>
2023-10-17 22:08:24 +0800

    update

---
+++ src/components/DiscoverFilter.vue
@@ -0,0 +1,82 @@
+<template>
+    <div class="filter stack" @click="handleClick">
+        <header>
+            <div>显示3955产品的1-18</div>
+            <div>
+                <a role="button">清除!!</a>
+            </div>
+        </header>
+        <div role="navigation" aria-label="类别">
+            <menu>
+                <li>
+                    <a href="/3d" aria-current="false" style="text-decoration: none;">3D<span
+                            class="icon icon-outline-cheveron-right"></span></a>
+                </li>
+                <li>
+                    <a href="/audio" aria-current="false" style="text-decoration: none;">声音的<span
+                            class="icon icon-outline-cheveron-right"></span></a>
+                </li>
+                <li>
+                    <a href="/business-and-money" aria-current="false" style="text-decoration: none;">商业与金钱<span
+                            class="icon icon-outline-cheveron-right"></span></a>
+                </li>
+                <li>
+                    <a href="/comics-and-graphic-novels" aria-current="false" style="text-decoration: none;">漫画与图画小说</a>
+                </li>
+                <li>
+                    <a href="/design" aria-current="false" style="text-decoration: none;">设计<span
+                            class="icon icon-outline-cheveron-right"></span></a>
+                </li>
+                <li>
+                    <a href="/drawing-and-painting" aria-current="false" style="text-decoration: none;">绘图和绘画<span
+                            class="icon icon-outline-cheveron-right"></span></a>
+                </li>
+                <li>
+                    <a href="/education" aria-current="false" style="text-decoration: none;">教育<span
+                            class="icon icon-outline-cheveron-right"></span></a>
+                </li>
+                <li>
+                    <a href="/fiction-books" aria-current="false" style="text-decoration: none;">小说书籍<span
+                            class="icon icon-outline-cheveron-right"></span></a>
+                </li>
+                <li>
+                    <a href="/films" aria-current="false" style="text-decoration: none;">电影<span
+                            class="icon icon-outline-cheveron-right"></span></a>
+                </li>
+                <li>
+                    <a href="/fitness-and-health" aria-current="false" style="text-decoration: none;">健身与健康<span
+                            class="icon icon-outline-cheveron-right"></span></a>
+                </li>
+            </menu>
+        </div>
+    </div>
+</template>
+<script setup lang="ts">
+import { ref, onMounted } from 'vue';
+import mitter from '@/utils/eventBus'
+
+const msg = ref('Hello, world!')
+
+const handleClick = () => {
+    console.log(123123213)
+  mitter.emit('msg', msg.value)
+}
+</script>
+<style scoped>
+    .filter * {
+        display: flex;
+        flex-wrap: wrap;
+        align-items: center;
+        padding: 1rem;
+        gap: 1rem;
+        justify-content: space-between;
+    }
+    .stack>* {
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    padding: 1rem;
+    gap: 1rem;
+    justify-content: space-between
+}
+</style>
--- src/components/HeaderNav.vue
+++ src/components/HeaderNav.vue
@@ -1,52 +1,21 @@
 <template>
-    <nav class="container mx-auto flex flex-row justify-between py-5 border-b-4">
-        <a class="logo text-2xl" href="/">YEN</a>
-        <ul class="flex flex-row">
-            <li v-for="link in navList" :key="link.link" class=" text-lg px-3">
-                <a :href="link.link">{{ link.text }}</a>
-            </li>
-        </ul>
-    </nav>
-    <button @click="increment">
-        {{ count }}
-    </button>
+  <div class="wrapper">
+    兄弟组件B
+    {{ res }}
+  </div>
 </template>
 
-<script setup>
-// import { ref } from 'vue';
-import { ref, onMounted } from 'vue'
-import axios from "axios";
-const count = ref(0)
+<script setup lang="ts">
+import mitter from '@/utils/eventBus'
+import { ref } from 'vue'
 
-function increment() {
-  count.value++
-}
+const res = ref()
 
-const props = defineProps({
-    titleName: String
-});
-
-onMounted(() => {
-    console.log(88888)
-
-
-    // 向给定ID的用户发起请求
-    axios.get('/user?ID=12345')
-    .then(function (response) {
-        // 处理成功情况
-        console.log(response);
-    })
-    .catch(function (error) {
-        // 处理错误情况
-        console.log(error);
-    })
-    .then(function () {
-        // 总是会执行
-    });
+mitter.on('msg', (data) => {
+  res.value = data
 })
-const navList = [
-    { text: 'ABOUT!!', link: 'about' },
-    { text: 'WORK', link: 'work' },
-]
-
 </script>
+
+<style scoped>
+
+</style>
